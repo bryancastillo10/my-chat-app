8bb90d2e734f740ef0766e1d890b91e7597ead0a
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-06-18 10:16:56 +0800

    jwt authentication complete

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>React Rendered</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// Local Imports
+import authRoutes from "./routes/auth.routes.js";
+import connectToDb from "./db/connectMongo.js";
+
+// Declared Variables
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// app.get("/", (req, res) => {
+//   res.send("Server is Ready");
+// });
+
+// MiddleWare
+app.use(express.json()); // to parse the incoming requests with JSON payloads
+app.use("/auth", authRoutes); //to designate a new API route
+
+app.listen(PORT, () => {
+  connectToDb();
+  console.log(`Listening on port ${PORT}`);
+});
