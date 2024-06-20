6efd15790e5f9931f8f0789425fe1b2f211cde9d
bryancastillo10 <bryanangelocastillo.10@gmail.com>
2024-06-20 21:56:25 +0800

    created Auth Context

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
 import Home from "./pages/home/Home";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 const App = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex  justify-center items-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={authUser ? <Home />: <Navigate to="/login"/>} />
+        <Route path="/login" element={authUser ? <Navigate to="/"/> : <Login/>} />
+        <Route path="/signup" element={authUser ? <Navigate to="/"/> : <SignUp/>} />
       </Routes>
       <Toaster/>
     </div>
