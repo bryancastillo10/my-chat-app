e3e3f9ede823a024c6c2c141eec0cb1c90975e58
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-06-28 23:08:52 +0800

    userUpdateApi

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,6 +3,7 @@ import {
   maleProfilePictures,
   femaleProfilePictures,
 } from "../utils/profilePictures.js";
+import { logoutUser } from "./auth.controller.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -21,7 +22,7 @@ export const getUsersForSidebar = async (req, res) => {
     res.status(200).json(allUsers);
   } catch (error) {
     console.log("Error in getUsersForSidebar controller", error.message);
-    res.status(500).json({ error: "Internal server  error" });
+    res.status(500).json({ error: "Something went wrong" });
   }
 };
 
@@ -53,7 +54,7 @@ export const profilePictureChoices = async (req, res) => {
     }
   } catch (error) {
     console.log("Error in profilePictureChoices controller", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Something went wrong" });
   }
 };
 
@@ -93,6 +94,79 @@ export const updateProfilePicture = async (req, res) => {
     });
   } catch (error) {
     console.log("Error in updateProfilePicture controller", error.message);
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+
+export const updateFullName = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const { fullName } = req.body;
+
+    const isExistingFullName = await User.findOne({
+      fullName,
+      _id: {
+        $ne: loggedInUserId
+      }
+    });
+    if (isExistingFullName) {
+      return res.status(400).json({ error: "Full Name already exist in this app" });
+    };
+
+    const updatedFullName = await User.findByIdAndUpdate(
+      loggedInUserId,
+      { fullName },
+      { new: true }
+    );
+    res.status(200).json({ updatedFullName });
+    
+  } catch (error) {
+    console.log("Error in updateFullName controller", error.message);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+}
+
+export const updateUserName = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+    const { username } = req.body;
+
+    const isExistingUsername = await User.findOne({
+      username,
+      _id: {
+        $ne: loggedInUserId
+      }
+    });
+
+    if (isExistingUsername) {
+      return res.status(400).json({ error: "Username already exist in this app" });
+    };
+    
+    const updatedUsername = await User.findByIdAndUpdate(
+      loggedInUserId,
+      { username },
+      { new: true }
+    );
+
+    res.status(200).json({ updatedUsername });
+
+  } catch (error) {
+    console.log("Error in updateUserName controller", error.message);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+export const deleteAccount = async (req, res) => {
+  try {
+    const userId = req.user._id;
+
+    await User.findByIdAndDelete(userId);
+
+    res.status(200).json({message:"Account has been deleted"});
+    logoutUser(req, res);
+  } catch (error) {
+    console.log("Error in delete account controller", error.message);
+    res.status(500).json({ error: "Something went wrong" });
   }
 };
