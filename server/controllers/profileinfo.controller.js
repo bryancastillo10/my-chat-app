edf91c892485b61a79ff71b6c21a532a7dd2a91e
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-06-28 21:40:45 +0800

    added profileInfoController

--- server/controllers/profileinfo.controller.js
+++ server/controllers/profileinfo.controller.js
@@ -1,13 +1,70 @@
-// import ProfInfo from "../models/profileinfo.model";
+import ProfInfo from "../models/profileinfo.model.js";
 
 export const addInfo = async (req, res) => {
-  console.log("Add Info Endpoint");
+  try {
+    const { birthday, hobbies, motto } = req.body;
+    const newProfileInfo = new ProfInfo({
+      birthday,
+      hobbies,
+      motto
+    });
+
+    if (hobbies & hobbies.length < 1 || hobbies.length > 3) {
+      return res.status(400).json({error:"Please select 1 up to 3 hobbies."})
+    };
+
+    await newProfileInfo.save();
+    res.status(201).json(newProfileInfo);
+  }
+  catch (error) {
+    console.log("Error in addInfo controller", error.message);
+    res.status(500).json({ error: "Something went wrong" });
+  }
 };
 
 export const updateInfo = async (req, res) => {
-  console.log("Update Info Endpoint");
+  try {
+    const loggedInId = req.params.id;
+    const { birthday, hobbies, motto } = req.body;
+
+
+
+    if (hobbies && (hobbies.length < 1 || hobbies.length > 3)) {
+      return res.status(400).json({ error: "Please select between 1 and 3 hobbies." });
+    }
+
+    const updatedProfile = await ProfInfo.findByIdAndUpdate(
+      loggedInId,
+      { birthday, hobbies, motto },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedProfile) {
+      return res.status(404).json({ error: "Profile information not found." });
+    }
+
+    res.status(200).json({
+      message: "Profile successfully updated",
+      updatedProfile: updatedProfile,
+    });
+  } catch (error) {
+    console.log("Error in updateInfo controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
 
 export const deleteInfo = async (req, res) => {
-  console.log("Delete Info Endpoint");
+  try {
+    const loggedInId = req.params.id;
+    const info = await ProfInfo.findByIdAndDelete(loggedInId);
+
+    if (!info) {
+      return res.status(404).json({ error: "Profile Info not found" });
+    }
+    res.status(200).json({message:"Profile Info has been deleted"});
+  }
+  catch (error) {
+    console.log("Error in deleteInfo controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 };
