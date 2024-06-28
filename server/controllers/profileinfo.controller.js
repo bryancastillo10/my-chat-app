2ea63ec07bc587d63e2b543633f1767168dc244e
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-06-28 16:12:24 +0800

    add API starting point for profile info

---
+++ server/controllers/profileinfo.controller.js
@@ -0,0 +1,13 @@
+import ProfInfo from "../models/profileinfo.model";
+
+export const addInfo = async (req, res) => {
+  console.log("Add Info Endpoint");
+};
+
+export const updateInfo = async (req, res) => {
+  console.log("Update Info Endpoint");
+};
+
+export const deleteInfo = async (req, res) => {
+  console.log("Delete Info Endpoint");
+};
---
+++ server/models/profileinfo.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const profInfoSchema = new mongoose.Schema(
+  {
+    birthday: {
+      type: Date,
+      default: "",
+    },
+    hobbies: {
+      type: [String],
+      required: false,
+      enum: [
+        "singing",
+        "dancing",
+        "reading",
+        "traveling",
+        "cooking",
+        "gardening",
+        "painting",
+        "writing",
+        "sports",
+        "photography",
+      ],
+    },
+    motto: {
+      type: String,
+      required: false,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const ProfInfo = mongoose.model("ProfInfo", profInfoSchema);
+
+export default ProfInfo;
