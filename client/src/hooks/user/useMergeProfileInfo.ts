db1073040a1a7db2a40db2d20cf1004bede46728
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-07-04 15:55:46 +0800

    revise mergeProfileInfo logic

--- client/src/hooks/user/useMergeProfileInfo.ts
+++ client/src/hooks/user/useMergeProfileInfo.ts
@@ -3,17 +3,39 @@ import { profileInfoData } from "../../store/useUpdateProfileModal";
 import useGetConversations from "../messages/useGetConversations";
 import useGetAllProfileInfo from "./useGetAllProfileInfo";
 
+export type MergedProfileInfo = Omit<
+  NonNullable<userData>,
+  "profileInfo" | "__v" | "createdAt" | "updatedAt"
+> & {
+  profileInfo: profileInfoData;
+};
+
 const useMergeProfileInfo = () => {
   const { conversations } = useGetConversations();
   const { allProfileInfo } = useGetAllProfileInfo();
 
-  const mergeProfileInfo = conversations.map((convo: userData) => {
-    const profileInfo = allProfileInfo.find(
-      (info: profileInfoData) => info._id === convo?.profileInfo
-    );
-    return { ...convo, profileInfo };
-  });
-  return { mergeProfileInfo };
+  const mergeProfileInfo: MergedProfileInfo[] = conversations
+    .filter((convo): convo is NonNullable<userData> => convo !== null)
+    .map((convo) => {
+      const profileInfo = allProfileInfo.find(
+        (info) => info._id === convo.profileInfo
+      );
+
+      if (!profileInfo) {
+        throw new Error(`Profile info not found for those users ${convo._id}`);
+      }
+
+      return {
+        _id: convo._id,
+        fullName: convo.fullName,
+        gender: convo.gender,
+        profilePic: convo.profilePic,
+        username: convo.username,
+        profileInfo: profileInfo,
+      };
+    });
+
+  return mergeProfileInfo;
 };
 
 export default useMergeProfileInfo;
