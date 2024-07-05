991818e1e1ff4dce714c87f5ba82808a0592159f
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-07-05 11:23:17 +0800

    added sound and chat bubble shaking

--- client/src/hooks/messages/useListenMessages.ts
+++ client/src/hooks/messages/useListenMessages.ts
@@ -2,20 +2,29 @@ import { useCallback, useEffect } from "react";
 import { useSocketContext } from "./useSocketContext";
 import useConversation from "../../store/useConversation";
 import { messageData } from "../../store/useConversation";
-
+import notification from "../../assets/notify.mp3";
 const useListenMessages = () => {
-    const { socket } = useSocketContext();
-    const { messages, setMessages } = useConversation();
+  const { socket } = useSocketContext();
+  const { messages, setMessages } = useConversation();
 
-    const handleNewMessage = useCallback((newMessage: messageData) => {
-        setMessages([...messages, newMessage])
-    }, [messages,setMessages]);
-    
-    useEffect(() => {
-       socket?.on("newMessage", handleNewMessage)		
-       return () => {socket?.off("newMessage")}
-    }
-    ,[socket, handleNewMessage]);
-}
+  const handleNewMessage = useCallback(
+    (newMessage: messageData) => {
+      if (newMessage) {
+        newMessage.shouldShake = true;
+      }
+      const audio = new Audio(notification);
+      audio.play();
+      setMessages([...messages, newMessage]);
+    },
+    [messages, setMessages]
+  );
+
+  useEffect(() => {
+    socket?.on("newMessage", handleNewMessage);
+    return () => {
+      socket?.off("newMessage");
+    };
+  }, [socket, handleNewMessage]);
+};
 
 export default useListenMessages;
