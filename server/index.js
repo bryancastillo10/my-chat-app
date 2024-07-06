319ba28428e76fce6fa3ef56d64e43e511adcbea
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-07-06 21:37:28 +0800

    server only default

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
+
 
 // Local Imports
 import authRoutes from "./routes/auth.routes.js";
@@ -15,7 +15,7 @@ import { app, server } from "./socket/socket.js";
 // Declared Variables
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+
 
 // Server Run Validation
 app.get("/", (req, res) => {
@@ -36,11 +36,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/profileinfo", profileRoutes);
-app.use(express.static(path.join(__dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
 
 
 server.listen(PORT, () => {
