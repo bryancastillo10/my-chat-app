0f3d33ea346908572c42b029bbd99db7e1fc0e7e
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-07-06 22:15:31 +0800

    rearrange express API

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-
 // Local Imports
 import authRoutes from "./routes/auth.routes.js";
 import messagesRoutes from "./routes/messages.routes.js";
@@ -16,20 +15,14 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-
-// Server Run Validation
-app.get("/", (req, res) => {
-  res.status(200).json({message:"SpaceChat App Server is here!"})
-});
-
 // Middleware
-app.use(express.json()); 
-app.use(cookieParser());
 app.use(cors({
   origin: "https://space-chat-beta.vercel.app",
   method:["POST","GET","PUT","DELETE"],
   credentials:true,
 }))
+app.use(express.json()); 
+app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
@@ -37,7 +30,10 @@ app.use("/api/messages", messagesRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/profileinfo", profileRoutes);
 
-
+// Server Run Validation
+app.get("/", (req, res) => {
+  res.status(200).json({message:"SpaceChat App Server is here!"})
+});
 
 server.listen(PORT, () => {
   connectToDb();
