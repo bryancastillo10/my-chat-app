2359edf946b61b12c8cea3d633248eb37b244323
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-07-06 16:07:31 +0800

    revise origin

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 const corsOptions = {
-  origin: 'https://spacechat-liart.vercel.app',
+  origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true, 
@@ -23,13 +23,13 @@ const corsOptions = {
 
 // Server Run Validation
 app.get("/", (req, res) => {
-  res.status(200).json({message:"SpaceChat App Server is here!"})
+  res.status(200).json({message:" App Server is here!"})
 });
 
 // Middleware
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
-app.use(express.json()); // to parse the incoming requests with JSON payloads
+app.use(express.json()); 
 app.use(cookieParser());
 app.use(cors({
   origin: "http://localhost:5173",
