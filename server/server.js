f4eff0bc59720b56720ff860600a62cb1d1250f6
bryancastillo10 <bryan.angelocastillo.10@gmail.com>
2024-07-08 06:46:00 +0800

    addedIClientURL

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ import { app, server } from "./socket/socket.js";
 // Declared Variables
 dotenv.config();
 const PORT = process.env.PORT || 5000;
-export const CLIENT = process.env.REACT_URL || "http://localhost:5173";
+const CLIENT = process.env.REACT_URL || "http://localhost:5173";
 
 // Test Run
 app.get("/", (req, res) => {
