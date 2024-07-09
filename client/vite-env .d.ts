f4cca00b8a4b1838f0aca73a1980ba6f0932b794
bryancastillo10 <bryanangelo.castillo.10@gmail.com>
2024-07-09 09:33:46 +0800

    assetType Declaration

--- client/vite-env .d.ts
+++ client/vite-env .d.ts
@@ -1,9 +1,10 @@
 /// <reference types="vite/client" />
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
 
 interface ImportMetaEnv {
-  readonly VITE_API_BASE_URL: string
+  readonly VITE_API_BASE_URL: string;
+  [key: string]: any;
 }
